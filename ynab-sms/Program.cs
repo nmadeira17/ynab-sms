38468e68026bbe6a73d7a91815ac71645cad81e2
Nick Madeira <nmadeira17.aux@gmail.com>
2021-03-23 16:26:37 -0700

    Initial commit

--- ynab-sms/Program.cs
+++ ynab-sms/Program.cs
@@ -1,19 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using CommandLine;
 
 namespace Ynab_Sms
 {
+    /// <summary>
+    /// Arguments to expect when the application is executed from the command line
+    /// </summary>
+    public class CommandLineOptions
+    {
+        [Value(index: 0, Required = true, HelpText = "Path to config file")]
+        public string ConfigFilePath { get; set; }
+    }
+
     class Program
     {
+        /// <summary>
+        /// Is what it is. Does what it does.
+        /// </summary>
         static void Main(string[] args)
         {
-            string budgetItemsJsonFile = "../Config/budget_items.json";
-            BudgetItemsConfig budgetConfig = BudgetItemsConfig.CreateFromJson(budgetItemsJsonFile);
-            if (budgetConfig == null)
-            {
-                return;
-            }
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
 
-            Console.WriteLine(budgetConfig.ToJson());
+        /// <summary>
+        /// Called when CommandLineParse was able to successfully parse the command line arguments
+        /// </summary>
+        static void RunOptions(CommandLineOptions options)
+        {
+            Runner.Run(options.ConfigFilePath);
+        }
+
+        /// <summary>
+        /// Called when CommandLineParse failed to parse the command line arguments
+        /// </summary>
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            Console.WriteLine("Error parsing command line arguments. Errors:\n");
+            foreach (Error e in errors)
+                Console.WriteLine(e.ToString());
         }
     }
 }
