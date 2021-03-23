801e28d26ae4055c25daca4949b9c84c1d5e0013
Nick Madeira <nmadeira17.aux@gmail.com>
2021-03-23 13:59:24 -0700

    Stand up directory structure

---
+++ ynab-sms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ynab_Sms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
